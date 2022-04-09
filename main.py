e8a3518655047e5c310fa831e7dc01e3aba8358f
YuFu Shen <tch3462@goo.tyai.tyc.edu.tw>
2022-04-09 14:36:45 +0800

    refresh link

--- main.py
+++ main.py
@@ -5,5 +5,10 @@ app = Flask(__name__, template_folder='template')
 @app.route("/")
 def home():
     return render_template('test.html')
+
+@app.route("/g-meet")
+def red():
+    return render_template('index.html')
+
 if __name__=="__main__":
     app.run(debug=True)
